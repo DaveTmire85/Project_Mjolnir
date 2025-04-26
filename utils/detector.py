40049a9a8c3c0c65f05c5b4d82f494d7e45b8465
Davetmire85 <david.tofflemire@student.uagc.edu>
2025-04-26 00:37:41 -0500

    new file:   db/mjolnir.db 	new file:   logs/mjolnir.log 	modified:   utils/detector.py

--- utils/detector.py
+++ utils/detector.py
@@ -4,7 +4,8 @@ from parser.spells_parser import SpellsParser
 from parser.items_parser import ItemsParser
 from parser.races_parser import RacesParser
 from parser.classes_parser import ClassesParser
-# Future: import monsters_parser, templates_parser when built
+# from parser.monsters_parser import MonstersParser  # Uncomment if you have the MonstersParser implemented
+# from parser.templates_parser import TemplatesParser  # Uncomment if you have the TemplatesParser implemented
 
 class EntityTypeDetector:
     def detect_parser(self, file_path):
@@ -13,20 +14,22 @@ class EntityTypeDetector:
             document = Document(file_path)
             first_page_text = "\n".join([p.text for p in document.paragraphs[:20]]).lower()
 
-            if "pantheon" in first_page_text or "domains" in first_page_text:
+            if "domains" in first_page_text or "deity" in first_page_text:
                 return DeitiesParser
             elif "prerequisite" in first_page_text and "benefit" in first_page_text:
                 return FeatsParser
-            elif "school" in first_page_text and "spell resistance" in first_page_text:
+            elif "casting time" in first_page_text and "school" in first_page_text:
                 return SpellsParser
             elif "cost" in first_page_text and "weight" in first_page_text:
                 return ItemsParser
-            elif "racial traits" in first_page_text or "favored class" in first_page_text:
+            elif "racial traits" in first_page_text or "size" in first_page_text:
                 return RacesParser
-            elif "class features" in first_page_text or "hit die" in first_page_text:
+            elif "hit die" in first_page_text or "base attack bonus" in first_page_text:
                 return ClassesParser
-            # Future: elif "hit dice" in first_page_text: (for monsters_parser)
-            # Future: elif "template traits" in first_page_text: (for templates_parser)
+#           elif "challenge rating" in first_page_text or "special attacks" in first_page_text:  # Uncomment if you have the MonstersParser implemented
+#               return MonstersParser
+#           elif "template" in first_page_text or "level adjustment" in first_page_text:  # Uncomment if you have the TemplatesParser implemented
+#               return TemplatesParser
 
         except Exception as e:
             print(f"[!] Failed to detect entity type for {file_path}: {e}")
